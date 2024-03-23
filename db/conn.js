7bdfdc685b05cc1a655300c457c826645afa1fb8
Giovani Murakami <giovani.murakami@outlook.com>
2024-03-23 16:23:23 -0300

    First Commit - Config Files

---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+    "lancheVital",
+    `${process.env.DB_USER}`,
+    `${process.env.DB_SECRET}`,
+    {
+        host: "localhost",
+        dialect: "mysql",
+    }
+);
+
+try {
+    sequelize.authenticate();
+    console.log("mysql ok");
+} catch (e) {
+    console.log(e);
+}
+
+module.exports = sequelize;
